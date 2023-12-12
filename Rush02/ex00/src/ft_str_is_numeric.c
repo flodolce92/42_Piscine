96d0bd5efce4ccd14eb0ef59bd2dd16346db246c
Federico Lo Dolce <flodolce92@gmail.com>
2023-12-12 21:20:03 +0100

    Rush02 v0.2

--- Rush02/ex00/src/ft_str_is_numeric.c
+++ Rush02/ex00/src/ft_str_is_numeric.c
@@ -1,33 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft.h                                               :+:      :+:    :+:   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: flo-dolc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/07 19:59:37 by flo-dolc          #+#    #+#             */
-/*   Updated: 2023/12/09 04:49:30 by flo-dolc         ###   ########.fr       */
+/*   Created: 2023/12/09 01:42:51 by flo-dolc          #+#    #+#             */
+/*   Updated: 2023/12/09 01:43:09 by flo-dolc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_H
-# define FT_H
+#include "ft.h"
 
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <fcntl.h>
-
-typedef struct s_dict
+int	ft_str_is_numeric(char *str)
 {
-	int				nb;
-	char			*wr_nb;
-	struct s_dict	*next;
-}	t_dict;
-
-int		ft_atoi(char *str);
-void	ft_putstr(char *str);
-int		ft_str_is_numeric(char *str);
-int		ft_strlen(char *str);
-
-#endif
+	while (*str != '\0')
+	{
+		if (*str < 48 || *str > 57)
+			return (0);
+		str++;
+	}
+	return (1);
+}
--- Rush02/ex00/src/main.c
+++ Rush02/ex00/src/main.c
@@ -6,11 +6,74 @@
 /*   By: flo-dolc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 19:28:24 by flo-dolc          #+#    #+#             */
-/*   Updated: 2023/12/08 02:40:48 by flo-dolc         ###   ########.fr       */
+/*   Updated: 2023/12/11 03:28:24 by flo-dolc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft.h"
+
+int	parse_dict(t_dict **headDict, char *str)
+{
+}
+
+char	*add_char(char *str, char buf)
+{
+	char	*temp;
+	int		strlen;
+	int		i;
+
+	i = 0;
+	strlen = ft_strlen(str);
+	temp = (char *) malloc(sizeof(char) * strlen + 2);
+	while (str[i])
+	{
+		temp[i] = str[i];
+		i++;
+	}
+	temp[i] = buf;
+	temp[i + 1] = '\0';
+	free(str);
+	return (temp);
+}
+
+int	read_file(int fd, t_dict **headDict)
+{
+	char	buf[1];
+	char	*str;
+
+	str = (char *) malloc(sizeof(char));
+	str[0] = '\0';
+	while (read(fd, buf, 1) != 0)
+	{
+		if (buf[0] != '\n')
+		{
+			str = add_char(str, buf[0]);
+		}
+		else
+		{
+			//ft_putstr("RIGA ");
+			//ft_putstr(str);
+			//ft_putstr("\n");
+			parse_dict(headDict, str);
+			free(str);
+			str = (char *) malloc(sizeof(char));
+			str[0] = '\0';
+		}
+	}
+	return (1);
+}
+
+int	open_file(char	*dictpath, t_dict **headDict)
+{
+	int	fd;
+
+	fd = open(dictpath, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	if (read_file(fd, headDict))
+		return (1);
+	return (0);
+}
 
 int	manage_argvs(int ac, char **av, char **dictpath, char **nb)
 {
@@ -29,12 +92,9 @@ int	manage_argvs(int ac, char **av, char **dictpath, char **nb)
 	return (0);
 }
 
-void	find_nb(char *dictpath, char *nb)
-{
-}
-
 int	main(int ac, char **av)
 {
+	t_dict	*dict;
 	char	*dictpath;
 	char	*nb;
 
@@ -45,15 +105,15 @@ int	main(int ac, char **av)
 	else
 	{
 		manage_argvs(ac, av, &dictpath, &nb);
-		if (ft_atoi(nb) != 0)
+		if (ft_str_is_numeric(nb) != 0)
 		{
 			ft_putstr("Prova ad aprire file\n");
-			open_file(dictpath);
-			ft_putstr("Trova Numero\n");
-			find_nb(dictpath, nb);
+			if (!open_file(dictpath, &dict))
+				return (-1);
 		}
 		else
 			ft_putstr("Error\n");
 	}
+	free(dict);
 	return (0);
 }
